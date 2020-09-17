9943ee3d731427acc7f7019dbe39941262648815
dECRISES <pratik.kumar.eee13@iitbhu.ac.in>
2020-09-17 12:46:53 +0530

    feat(manage-groups): lazily loaded manage-groups module in the root route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('../../projects/home/src/app/home.module').then((m) => m.HomeModule),
   },
+  {
+    path: 'manage-groups',
+    loadChildren: () => import('../../projects/manage-groups/src/app/manage-groups.module').then((m) => m.ManageGroupsModule),
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home' },
 ];

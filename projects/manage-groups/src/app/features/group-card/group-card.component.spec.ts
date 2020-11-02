be5c61dbba9884fc6f415769cc032b93bd47dab9
dECRISES <pratik.kumar.eee13@iitbhu.ac.in>
2020-11-02 09:17:39 +0530

    test(group_card): fixed test failures

--- projects/manage-groups/src/app/features/group-card/group-card.component.spec.ts
+++ projects/manage-groups/src/app/features/group-card/group-card.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GroupCardComponent } from './group-card.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ManageGroupsModule } from '../../manage-groups.module';
 
 describe('GroupCardComponent', () => {
   let component: GroupCardComponent;
@@ -8,6 +11,7 @@ describe('GroupCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterModule.forRoot([]), HttpClientModule, ManageGroupsModule],
       declarations: [GroupCardComponent],
     }).compileComponents();
   });
